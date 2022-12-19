c6ae1a2be2f3deca642be9433687553b4750c9c7
Mohammad Ibrahim <112249309+awesomecoderdev@users.noreply.github.com>
2022-12-19 23:28:36 +0600

    Startup Project

--- ac_pintrest_downloader.php
+++ ac_pintrest_downloader.php
@@ -16,9 +16,9 @@
  * Plugin Name:       Pinterest  Video Downloader
  * Plugin URI:        https://awesomecoder.dev/
  * Description:       This is a custom plugin, You can use it for give access to user to download Pinterest Video :)
- * Version:           1.0.0
+ * Version:           1.0.1
  * Author:            Mohammad Ibrahim
- * Author URI:        hhttps://awesomecoder.dev/
+ * Author URI:        https://awesomecoder.dev/
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  * Text Domain:       ac_pintrest_downloader
