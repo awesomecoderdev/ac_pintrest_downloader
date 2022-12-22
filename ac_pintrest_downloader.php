bef0948dd9c27cf19edc9fbea61ff03e6743bd89
Mohammad Ibrahim <112249309+awesomecoderdev@users.noreply.github.com>
2022-12-22 19:30:18 +0600

    Updated Domain

--- ac_pintrest_downloader.php
+++ ac_pintrest_downloader.php
@@ -8,7 +8,7 @@
  * registers the activation and deactivation functions, and defines a function
  * that starts the plugin.
  *
- * @link              https://awesomecoderss.dev/
+ * @link              https://awesomecoderdev.github.io/
  * @since             1.0.0
  * @package           Ac_pintrest_downloader
  *
